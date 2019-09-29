904647f771393c19314de45bbd80dbe86c352843
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-28 23:28:19 -0700

    styling

--- sec03_Pros_and_More/Slot_Machine/machine.js
+++ sec03_Pros_and_More/Slot_Machine/machine.js
@@ -3,14 +3,13 @@ class Machine extends React.Component {
     render(){
         console.log(this.props);
         const {s1, s2, s3} = this.props;
+        const colors = {fontSize: '50px', backgroundColor: 'blue'};
         const winner = (s1 === s2) && (s2 === s3);
         return (
-            <div>
-                <div>
-                   <p>{s1}{s2}{s3}</p>
-                   <p>{winner ? 'Winner!' : 'Loser!'}</p>
-                </div>
-                
+            <div className='machine'>
+                   <p style={colors}>{s1}{s2}{s3}</p>
+                   <p className={winner ? 'win' : 'loses'}>
+                        {winner ? 'Winner!' : 'Loser!'}</p>
             </div>
         );    
     }
