4786cfb1ffdccea77ffe52c93b62a881ae0b22ba
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-28 23:00:07 -0700

    default props

---
+++ sec03_Pros_and_More/Default_Props/index.js
@@ -0,0 +1,21 @@
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Hello 
+                    from = "Carle"  
+                    to = "Dawn" 
+                    bangs = {4}
+                    img = "https://images.unsplash.com/photo-1436076863939-06870fe779c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    data = {[1,2,3,4,5]}
+                    isFunny 
+                />
+                <Hello 
+                    from = "Tom"
+                />
+            </div>
+        )
+    };
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
