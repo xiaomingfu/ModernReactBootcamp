2603925580221b9f5ae737f6b974beda1f3ae2c1
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-30 17:37:12 -0700

    update

--- sec06_intro_State/state-intro/src/NumGame.js
+++ sec06_intro_State/state-intro/src/NumGame.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 class NumGame extends Component{
     constructor(props){
         super(props);
-        this.state = {num: 0};
+        this.state = {num: 1};
         this.click = this.click.bind(this);
     }
     click(e){
-        let rand = Math.floor(Math.random() * 10);
+        let rand = Math.floor(Math.random() * 10) + 1;
         this.setState({num: rand});
     }
     render(){
@@ -15,8 +15,9 @@ class NumGame extends Component{
             <div>
                 <h1>Number is: {this.state.num} </h1>
                 <div>
-                    <button onClick={this.click}>Random Number</button>
-                    <h1>{this.state.num === 7 ? "You win!" : <button></button> }</h1>
+                    {this.state.num === 7 
+                        ? <h2>You Win!</h2> 
+                        : <button onClick={this.click}>Random Number</button> }
                 </div>
             </div>
         )
