1d1a8cffdad60727b9a393bd9fd098ddfedb2c0b
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-30 17:02:16 -0700

    add onClick event state use bind

--- sec06_intro_State/state-intro/src/App.js
+++ sec06_intro_State/state-intro/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import Game from './Game';
 import Rando from './Rando';
 import Button from './Button';
+import BrokenClick from './BrokenClick';
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <BrokenClick />
     </div>
   );
 }
