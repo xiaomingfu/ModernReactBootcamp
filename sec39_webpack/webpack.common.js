86e46286ec80341745f68c628890c227bb7aa081
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-27 00:58:52 -0700

    add vender entry

--- sec39_webpack/webpack.common.js
+++ sec39_webpack/webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: { main: "./src/index.js", vender: "./src/vender.js" },
   plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
