3abc9890827fe203a5772716b6ce0836590e1c54
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-26 23:47:50 -0700

    add dev config and production config

--- sec39_webpack/src/index.js
+++ sec39_webpack/src/index.js
@@ -6,5 +6,5 @@ import { ComponentService } from "./app/component.service";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
-
+console.log("Hi");
 run(alertService, componentService);
--- sec39_webpack/webpack.common.js
+++ sec39_webpack/webpack.common.js
@@ -1,12 +1,9 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
