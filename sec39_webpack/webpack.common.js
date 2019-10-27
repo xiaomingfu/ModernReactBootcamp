e6560d974a81d85c61fcfd1f77bce59618b42eeb
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-27 01:41:33 -0700

    minify css js html

--- sec39_webpack/webpack.common.js
+++ sec39_webpack/webpack.common.js
@@ -1,12 +1,10 @@
 const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: { main: "./src/index.js", vender: "./src/vender.js" },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+
   module: {
     rules: [
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
       { test: /\.html$/, use: ["html-loader"] },
       {
         test: /\.(svg|png|gif|jpg)$/,
